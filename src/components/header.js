9e7a7af663fd0844d94403de809989c85c23a827
wehearyoudeveloper <wehearyou.developer@gmail.com>
2021-02-17 22:27:51 -0500

    on the right track

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,109 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+  display: flex;
+  padding: 15px;
+  justify-content: space-between;
+  align-items: center;
+
+  @media only screen and (max-width: 680px) {
+    display: block;
+  }
+`
+const UlStyled = styled.ul`
+  @media only screen and (max-width: 680px) {
+    text-align: center;
+  }
+`
+const LiStyled = styled.li`
+  display: inline;
+  margin-right: 15px;
+
+  &:last-child {
+    @media only screen and (max-width: 680px) {
+      position: fixed;
+      margin: 0;
+      padding: 20px 0;
+      width: 100%;
+      bottom: 0;
+      left: 0;
+      z-index: 5;
+      background-color: var(--lightOrange);
+    }
+  }
+`
+const LinkStyled = styled(Link)`
+  text-decoration: none;
+  color: var(--black);
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  @media only screen and (max-width: 680px) {
+    text-decoration: underline;
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+`
+const LinkStyledButton = styled(Link)`
+  border: none;
+  padding: 10px 30px;
+  background: var(--orange);
+  border-radius: 50px;
+  font-size: 1.05em;
+  text-decoration: none;
+  color: var(--black);
+
+  &:hover {
+    background: white;
+    border: 1px solid var(--orange);
+    color: orange;
+    padding: 9px 29px;
+  }
+
+  @media only screen and (max-width: 680px) {
+    padding: 10px 40px;
+
+    &:hover {
+      padding: 10px 40px;
+    }
+  }
+`
+const BackgroundColorPiece = styled.div`
+  position: absolute;
+  width: 40%;
+  height: 80vh;
+  background-color: var(--lightOrange);
+  z-index: -1;
+  top: 0;
+  left: 0;
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <StyledHeader>
+    <h1>Pierre Paul</h1>
+    <UlStyled>
+      <LiStyled>
+        <LinkStyled to="/">Speaking</LinkStyled>
+      </LiStyled>
+      <LiStyled>
+        <LinkStyled to="/">Well Pierre</LinkStyled>
+      </LiStyled>
+      <LiStyled>
+        <LinkStyled to="/">Resume</LinkStyled>
+      </LiStyled>
+      <LiStyled>
+        <LinkStyledButton to="/">Contact</LinkStyledButton>
+      </LiStyled>
+    </UlStyled>
+    <BackgroundColorPiece />
+  </StyledHeader>
 )
 
 Header.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,199 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
+import { AnchorLink } from "gatsby-plugin-anchor-links"
+import Slider from "react-slick"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+import Img from "gatsby-image"
+
+const HeroSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 15px;
+
+  @media only screen and (max-width: 680px) {
+    display: block;
+  }
+`
+const StyledImg = styled(Img)`
+  width: 60%;
+  max-width: 500px;
+  min-width: 300px;
+  bottom: 0;
+`
+const DivStyled = styled.div`
+  max-width: 600px;
+
+  @media only screen and (max-width: 680px) {
+    width: 100%;
+    margin: auto;
+  }
+`
+const PStyled = styled.p`
+  margin-left: 15px;
+  max-width: 35em;
+
+  @media only screen and (max-width: 680px) {
+    margin-left: 0;
+    margin-bottom: 25px;
+  }
+`
+const FloatingBallOne = styled.div`
+  background-color: var(--lightOrange);
+  height: 100px;
+  width: 100px;
+  min-width: 100px;
+  border-radius: 200px;
+  position: absolute;
+  top: 30%;
+  right: 30%;
+  z-index: -1;
+`
+const FloatingBallTwo = styled.div`
+  background-color: var(--lightOrange);
+  height: 50px;
+  width: 50px;
+  min-width: 50px;
+  border-radius: 50px;
+  position: absolute;
+  top: 25%;
+  right: 10%;
+  z-index: -1;
+`
+const FloatingBallThree = styled.div`
+  background-color: var(--lightOrange);
+  height: 25px;
+  width: 25px;
+  min-width: 25px;
+  border-radius: 25px;
+  position: absolute;
+  top: 15%;
+  right: 30%;
+  z-index: -1;
+`
+const SpeakingSection = styled.section`
+  background-color: var(--lightBlue);
+  width: 100%;
+  padding: 100px;
+  box-sizing: border-box;
+
+  @media only screen and (max-width: 680px) {
+    padding: 25px 15px;
+  }
+`
+const FlexDiv = styled.div`
+  display: flex;
+  align-items: center;
+  max-width: 960px;
+  margin: 0 auto;
+
+  @media only screen and (max-width: 680px) {
+    display: block;
+  }
+`
+const BoxDiv = styled.div`
+  background-color: white;
+  width: 40%;
+  margin-right: 100px;
+  padding: 0px 30px 0px 20px;
+
+  @media only screen and (max-width: 680px) {
+    width: 80%;
+    margin: 0 auto;
+  }
+`
+const BlockText = styled.p`
+  max-width: 200px;
+`
+
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Pierre Paul | Passionate Speaker" />
+      <HeroSection>
+        <StyledImg fluid={data.fileName.childImageSharp.fluid} />
+        <DivStyled>
+          <h2>Poised &amp; Passionate</h2>
+          <PStyled>
+            CEO and MBA candidate, Pierre Paul, began speaking for audiences in
+            high school. After positioning himself as a top speaker in the USA,
+            Pierre’s bringing his voice to public and private stages. Please
+            contact Pierre directly for speaking requests.
+          </PStyled>
+        </DivStyled>
+        <FloatingBallOne />
+        <FloatingBallTwo />
+        <FloatingBallThree />
+      </HeroSection>
+      <SpeakingSection>
+        <FlexDiv>
+          <DivStyled>
+            <h2 style={{marginTop: 0,}}>Varied &amp; Engaging</h2>
+            <p>
+              Pierre speaks on a variety of topics and creates a vibrant space
+              for people to learn and ask questions. Some of the subject matter
+              Pierre talks on includes race, religion, social justice, politics,
+              music, history, his immigration experience, and change.
+            </p>
+          </DivStyled>
+
+        </FlexDiv>
+      </SpeakingSection>
+      <SpeakingSection style={{ backgroundColor: "white" }}>
+        <div className="video-container">
+          <iframe
+            src="https://www.youtube.com/embed/QsiJOxmyK7Q"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <div className="video-container">
+          <iframe
+            src="https://www.youtube.com/embed/nY2xBD4jxfI"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </SpeakingSection>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    fileName: file(relativePath: { eq: "Pierre-Tall-Compressed.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
+
+/**
+ * <SpeakingSection>
+      <FlexDiv>
+        <BoxDiv>
+          <h2>
+            Race. Religion. Social Justice. Politics. Music. Black History.
+            Immigrating. Change.
+          </h2>
+        </BoxDiv>
+        <PStyled>
+          Pierre speaks on a variety of topics and is open to taking on new
+          subject matter.
+        </PStyled>
+      </FlexDiv>
+    </SpeakingSection>
+ */
